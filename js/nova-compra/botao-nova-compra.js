484dea678aa4f88f8a9005b8ac6876d085cd9b2e
unknown <daniel_fcampos@hotmail.com>
2021-01-07 11:11:28 -0300

    botao nova compra

--- js/nova-compra/botao-nova-compra.js
+++ js/nova-compra/botao-nova-compra.js
@@ -1,10 +1,52 @@
+// variáveis do DOM
 var compraProdutoTabela = document.getElementById ("compraProdutoRow");
 var botaoNovaCompra = document.querySelector (".nova-compra-botao");
-var rowCount = compraProdutoTabela.rows.length;
+var tabelaCompras = document.getElementById('compraFornecedorTable');
+// pegando a data de hoje
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0');
+var yyyy = today.getFullYear();
+today = dd + '/' + mm + '/' + yyyy;
 
 //quando botao clicado
 botaoNovaCompra.addEventListener("click", function(){
-    for (i= 1; i < rowCount; i++) {
-        compraProdutoTabela.deleteRow(1);
+    // limpando a tabela central
+    var rowCount = compraProdutoTabela.rows.length;
+    if (rowCount > 1) {
+        for (i= 1; i < rowCount; i++) {
+            compraProdutoTabela.deleteRow(1);
+        }
     }
+    else {
+        console.log("já existe uma nova compra vazia")
+        return;
+    }
+    //recontando o rowCount para entrar no else na próxima vez
+    rowCount = compraProdutoTabela.rows.length;
+
+    console.log(today);
+    // criando os elementos na coluna esquerda
+    var novaCompra = document.createElement('tr');
+    var nomeCompra = document.createElement('td')
+    var dataCompra = document.createElement('td')
+
+    // colocando os dados nos elementos
+    nomeCompra.innerHTML = "Nova Compra";
+    dataCompra.innerHTML = today;
+
+    novaCompra.appendChild(nomeCompra);
+    novaCompra.appendChild(dataCompra);
+    tabelaCompras.appendChild(novaCompra);
+    limpaNegrito();
+    novaCompra.classList.add("compra-fornecedor-selecionada");
+    novaCompra.classList.add('compra-tr');
 });
+
+// limpa negrito da compra selecionada
+function limpaNegrito () {
+    var comprasRow = document.querySelectorAll('.compra-tr');
+    comprasRow.forEach (item => {
+        item.classList.remove("compra-fornecedor-selecionada")
+    })
+}
